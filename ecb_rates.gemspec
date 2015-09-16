66c52c33390b12b58c242ba6bdc8cbc0f25c2d04
ashut-pc <artemshut@gmail.com>
2015-09-16 21:34:03 +0300

    Add couple tests and some refactoring

--- ecb_rates.gemspec
+++ ecb_rates.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |spec|
   spec.name          = "ecb_rates"
-  spec.version       = "0.0.0"
+  spec.version       = "0.0.1"
   spec.authors       = ["Artsiom Shut"]
   spec.email         = ["artemshut@gmail.com"]
   spec.description   = %q{ECB rates for UOL.}
@@ -8,8 +8,11 @@ Gem::Specification.new do |spec|
   spec.homepage      = "https://github.com/artemshut/ecb_rates"
   spec.license       = "MIT"
 
-  spec.files         = ["lib/ecb_rates.rb"]
-  spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
+  spec.files         = `git ls-files`.split("\n")
+  spec.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
+  spec.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
+  spec.add_development_dependency 'rake', '~> 0'
+  spec.add_development_dependency 'rspec', '~> 0'
 end
--- lib/ecb_rates.rb
+++ lib/ecb_rates.rb
@@ -1,45 +1,56 @@
-require 'nokogiri'
 require 'net/http'
-require 'active_support/core_ext/hash'
 require 'uri'
+require 'active_support/core_ext/hash'
 require 'date'
 
 class ECBRates
+
   ECB_URL_DAILY = 'http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml'
   ECB_URL_90 = 'http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml'
   ECB_URL_FROM_BEGINNING = 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.xml'
-  #TODO: add extension to check allowed currencies
+
   class << self
 
     def rates_for currency, date = Date.today
-      if date > Date.today
-        puts "The date should be in the past"
+      converted_date = convert date
+      if converted_date > Date.today.to_s
+        "######## The date should be in the past. ########"
       else
-        get_rate_of currency.upcase, date
+        get_rate_for currency.upcase, converted_date
       end
     end
 
-    def get_rate_of currency, date
+    def get_rate_for currency, converted_date
+      rates_for_period = Hash.from_xml(get_rates_from_ecb)
+      rates_for_chosed_date = rates_for_period["Envelope"]["Cube"]["Cube"].detect { |f| f["time"] == converted_date }
+      if rates_for_chosed_date
+        rate_with_currency = rates_for_chosed_date["Cube"].detect { |f| f["currency"] == currency }
+        if rate_with_currency && rate_with_currency["rate"]
+          rate_with_currency["rate"].to_f
+        else
+          "######## Service have no such currency. Check it please. ########"
+        end
+      else
+        "######## No rate for this date. May be it was a weekend? ########"
+      end
+    end
+
+    #
+    # Return xml with response from ECB
+    #
+    def get_rates_from_ecb
       uri = URI.parse ECB_URL_90
       http = Net::HTTP.new uri.host, uri.port
       request = Net::HTTP::Post.new uri.path
       response = http.request request
-      hash_of_currencies = Hash.from_xml(response.body)
-
-      #TODO: check if date == date.today not convert
-      query_date = Date.parse(date).to_s
-      hash_by_day = hash_of_currencies["Envelope"]["Cube"]["Cube"].detect { |f| f["time"] == query_date }
-      if hash_by_day.present?
-        rates_list = hash_by_day["Cube"].detect { |f| f["currency"] == currency }
-        if rates_list.present?
-          rates_list["rate"].to_f
-        else
-          puts "No needed currency in list. Try another one."
-        end
-      else
-        puts "There was a weekend on chosed date"
-      end
+      response.body
     end
+
+    #
+    # Convert date from parameters into a version, acceptable by ECB service
+    #
+    def convert date; date.is_a?(String) ? Date.parse(date).to_s : date.to_s end
+
   end
 
 end
