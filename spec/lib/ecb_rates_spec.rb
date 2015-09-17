6a1a50084a715da9cc4f26d2baad046acb2d70e4
ashut-pc <artemshut@gmail.com>
2015-09-17 23:26:00 +0300

    Update tests

--- spec/lib/ecb_rates_spec.rb
+++ spec/lib/ecb_rates_spec.rb
@@ -2,22 +2,22 @@ require 'spec_helper'
 
 describe ECBRates do
   it "should return valid currency parameter for 14.09.2015" do
-    rate_for_usd = ECBRates.rates_for 'usd', '14.09.2015'
+    rate_for_usd = ECBRates.rate 'usd', '14.09.2015'
     expect(rate_for_usd).to eq(1.1305)
   end
 
   it "should return nil if weekend" do
-    rate_for_usd = ECBRates.rates_for 'usd', '12.09.2015'
+    rate_for_usd = ECBRates.rate 'usd', '12.09.2015'
     expect(rate_for_usd).to be_nil
   end
 
   it "should return nil if incorrect currency" do
-    rate_for_usd = ECBRates.rates_for 'xxx', '14.09.2015'
+    rate_for_usd = ECBRates.rate 'xxx', '14.09.2015'
     expect(rate_for_usd).to be_nil
   end
 
   it "should return nil if incorrect date" do
-    rate_for_usd = ECBRates.rates_for 'usd', Date.today + 20.years
+    rate_for_usd = ECBRates.rate 'usd', Date.today + 20.years
     expect(rate_for_usd).to be_nil
   end
 end
