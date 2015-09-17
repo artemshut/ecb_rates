bcf2708796be945f5d6123af991dab1e6de123e0
Artem <artemshut@gmail.com>
2015-09-17 11:02:00 +0300

    Update ecb_rates_spec.rb

--- spec/lib/ecb_rates_spec.rb
+++ spec/lib/ecb_rates_spec.rb
@@ -6,17 +6,17 @@ describe ECBRates do
     expect(rate_for_usd).to eq(1.1305)
   end
 
-  it "should return weekend message" do
+  it "should return nil if weekend" do
     rate_for_usd = ECBRates.rates_for 'usd', '12.09.2015'
     expect(rate_for_usd).to be_nil
   end
 
-  it "should return incorrect currency message" do
+  it "should return nil if incorrect currency" do
     rate_for_usd = ECBRates.rates_for 'xxx', '14.09.2015'
     expect(rate_for_usd).to be_nil
   end
 
-  it "should return incorrect date message" do
+  it "should return nil if incorrect date" do
     rate_for_usd = ECBRates.rates_for 'usd', Date.today + 20.years
     expect(rate_for_usd).to be_nil
   end
