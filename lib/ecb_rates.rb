9684f0ab7d7b386b486d12311b881290197d7ad3
Artem <artemshut@gmail.com>
2015-11-09 11:59:31 +0300

    Update ecb_rates.rb

--- lib/ecb_rates.rb
+++ lib/ecb_rates.rb
@@ -7,9 +7,9 @@ require 'logger'
 class ECBRates
 
   # URLs for daily/90 days/from 1999 response after calling ECB
-  ECB_URL_DAILY = 'http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml'.freeze
-  ECB_URL_90 = 'http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml'.freeze
-  ECB_URL_FROM_BEGINNING = 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.xml'.freeze
+  ECB_URL_DAILY = 'http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml'
+  ECB_URL_90 = 'http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml'
+  ECB_URL_FROM_BEGINNING = 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.xml'
 
   class << self
 
